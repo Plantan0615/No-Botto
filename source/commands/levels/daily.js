96d1491bbc5f92089b42e3aeb5afa6cb6126788b
discord-bot-maker <59079717+discord-bot-maker@users.noreply.github.com>
2020-07-26 19:03:27 +0100

    resolve

--- source/commands/levels/daily.js
+++ source/commands/levels/daily.js
@@ -1,3 +1,14 @@
 module.exports.run = async(client, message, args) => {
-    message.reply("I am coming soon");
+    let xpFile = await fs.readFile("userXP.json", "utf8");
+    let xpObject = JSON.parse(xpFile);
+    if(xpObject.hasOwnProperty(message.author.id)){
+        let userXpObject = xpObject[message.author.id];
+        if (userXpObject.hasOwnProperty(message.guild.id)){
+            let guildXpObject = userXpObject[message.guild.id];
+            let currentMoneys = guildXpObject["userMoneys"];
+            let newMoneys = 200
+            let updatedMoneys = currentMoneys + newMoneys;
+
  }
+}
+}
--- source/commands/levels/stats.js
+++ source/commands/levels/stats.js
@@ -1,3 +1,26 @@
 module.exports.run = async(client, message, args) => {
-    message.reply("I am coming soon");
- }
+    let xpFile = await fs.readFile("userXP.json", "utf8");
+    let xpObject = JSON.parse(xpFile);
+    if(xpObject.hasOwnProperty(message.author.id)){
+        let userXpObject = xpObject[message.author.id];
+        if (userXpObject.hasOwnProperty(message.guild.id)){
+            let guildXpObject = userXpObject[message.guild.id];
+                let username = guildXpObject["username"];
+                let currentXp = guildXpObject["userXP"];
+                let currentMoneys = guildXpObject["userMoneys"];
+                let currentLevel = guildXpObject["userLevel"];
+                const statsEmbed = new MessageEmbed()
+                .setColor('RED')
+                .setTitle('Stats')
+                .setDescription(`User Stats for ${message.author.username}`)
+                .addFields(
+                    { name: "Username", value: username},
+                    { name: "XP", value: currentXp},
+                    { name: "Level", value: currentLevel},
+                    { name: "Moneys", value: currentMoneys}
+                );
+            message.channel.send(statsEmbed);
+            }
+    }
+}
+
