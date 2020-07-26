2271ca75e95b8a68561541f016d9d45313802efb
discord-bot-maker <59079717+discord-bot-maker@users.noreply.github.com>
2020-07-26 17:56:31 +0100

    notes

---
+++ source/commands/useful/find copy.js
@@ -0,0 +1,45 @@
+module.exports.run = async(client, message, args) => {
+    //getting announcement
+    let user = message.author
+    let announcement = `${user} This is the channel you were looking for`
+    //declare channnels
+    //
+    // You will need to declare each channel here and change the variable names in the if else statements
+    //
+    let generalChannel = client.channels.cache.find(channel => channel.name.toLowerCase() === "general")
+    let test1Channel = client.channels.cache.find(channel => channel.name.toLowerCase() === "test-1");
+    let test2Channel = client.channels.cache.find(channel => channel.name.toLowerCase() === "test-2");
+    let roleMenu = client.channels.cache.find(channel => channel.name.toLowerCase() === "role-menu");
+    //send message based on request
+    let request = message.content.toLowerCase().substring(6);
+    if(request === "test 1"){
+        message.delete();
+        test1Channel.send(announcement)
+        .then(msg => msg.delete({timeout: 15000}))
+        .catch(console.error);
+    }
+    else if(request === "test 2"){
+        message.delete();
+        test2Channel.send(announcement)
+        .then(msg => msg.delete({timeout: 15000}))
+        .catch(console.error);
+    }
+    else if(request === "general"){
+        message.delete();
+        generalChannel.send(announcement)
+        .then(msg => msg.delete({timeout: 15000}))
+        .catch(console.error);
+    }
+    else if(request === "role menu"){
+        message.delete();
+        roleMenu.send(announcement)
+        .then(msg => msg.delete({timeout: 15000}))
+        .catch(console.error);
+    }
+    else {
+        message.delete({timeout: 5000})
+        message.channel.send("Invalid Channel. Please search for a different channel.")
+        .then(msg => msg.delete({timeout: 5000}))
+        .catch(console.error);
+}
+};
---
+++ source/events/reactions/messageReactionAdd copy.js
@@ -0,0 +1,33 @@
+module.exports = (client, reaction, user) => {
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+    if(reaction.message.partial) reaction.message.fetch();
+    if(reaction.partial) reaction.fetch();
+    //
+    //tthe emoji IDs and channel ID must be hardcoded for this to work as it is adding roles based on what emoji was selected
+    //
+    if (reaction.message.channel.id === "734791204880777308") { 
+    try{
+    let member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+    //aww
+    let msg = reaction.message.fetch();
+    if (reaction.emoji.id === '734791623384236182'){
+        member.roles.add("734792750838448148");
+        console.log("Role Added");  
+    }
+    //binoculars
+    if (reaction.emoji.id === '734791626945200159'){
+        member.roles.add("734792813828505721");
+        console.log("Role Added");  
+    }
+    //hebeeg
+    if (reaction.emoji.id === '734791627830460456'){
+        member.roles.add("734792775580516363");
+        console.log("Role Added");  
+    } 
+}
+catch (err) {
+        console.log(err);
+    }
+}
+}
