0c76daf7ee38cf1174356c27133c6ad2106013d0
discord-bot-maker <59079717+discord-bot-maker@users.noreply.github.com>
2020-07-26 18:37:44 +0100

    excluded discord modules

--- source/commands/levels/stats.js
+++ source/commands/levels/stats.js
@@ -1,4 +1,15 @@
 const userxp = require("/Users/chase/Desktop/Coding/No Botto/source/userXP.json");
 module.exports.run = async(client, message, args) => {
-    
+    let xpFile = await fs.readFile("userXP.json", "utf8");
+    let xpObject = JSON.parse(xpFile);
+    if(xpObject.hasOwnProperty(message.author.id)){
+        let userXpObject = xpObject[message.author.id];
+        if (userXpObject.hasOwnProperty(message.guild.id)){
+        let guildXpObject = userXpObject[message.guild.id];
+        let username = guildXpObject["username"];
+        let currentXp = guildXpObject["userXP"];
+        let currentMoneys = guildXpObject["userMoneys"];
+        let currentLevel = guildXpObject["userLevel"];
+    }
+}
 }
