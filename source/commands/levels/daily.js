1b6a8fe726a979af4ff5978cb275a28098ca79d0
discord-bot-maker <59079717+discord-bot-maker@users.noreply.github.com>
2020-07-27 02:03:22 +0100

    stuff
    finisdhed stats, dice, currently fixing level role assignment

--- source/commands/levels/daily.js
+++ source/commands/levels/daily.js
@@ -1,16 +1,28 @@
 const userxp = require("/Users/chase/Desktop/Coding/No Botto/source/userXP.json");
 const fs = require("fs").promises;
+const talkedRecently = new Set();
 module.exports.run = async(client, message, args) => {
-    let xpFile = await fs.readFile("userXP.json", "utf8");
-    let xpObject = JSON.parse(xpFile);
-    if(xpObject.hasOwnProperty(message.author.id)){
-        let userXpObject = xpObject[message.author.id];
-        if (userXpObject.hasOwnProperty(message.guild.id)){
-            let guildXpObject = userXpObject[message.guild.id];
-            let currentMoneys = guildXpObject["userMoneys"];
-            let newMoneys = 200
-            let updatedMoneys = currentMoneys + newMoneys;
-            
- }
+    if (talkedRecently.has(message.author.id)) {
+        message.channel.send(`Wait until tomorrow before getting typing this again ${message.author.username}.`);
+}
+    else{
+        let xpFile = await fs.readFile("userXP.json", "utf8");
+        let xpObject = JSON.parse(xpFile);
+        if(xpObject.hasOwnProperty(message.author.id)){
+            let userXpObject = xpObject[message.author.id];
+            if (userXpObject.hasOwnProperty(message.guild.id)){
+                    let guildXpObject = userXpObject[message.guild.id];
+                    let currentMoneys = guildXpObject["userMoneys"];
+                    let newMoneys = 200
+                    let updatedMoneys = currentMoneys + newMoneys;
+                    xpObject[message.author.id][message.guild.id]["userMoneys"] = updatedMoneys;
+                    await fs.writeFile("userXP.json", JSON.stringify (xpObject, null, 4), "utf8")
+                    .then(message.channel.send("Daily Complete. Have 200 Moneys."))
+                    .catch(err=> console.log(err));      
+            }
+        }
+    talkedRecently.add(message.author.id);
+    setTimeout(() => {talkedRecently.delete(message.author.id);}, 86400000);
+    console.log(talkedRecently);
 }
 }
--- source/events/init/message.js
+++ source/events/init/message.js
@@ -25,7 +25,7 @@ module.exports = async(client, message) => {
             let userXpObject = xpObject[message.author.id];
             if (userXpObject.hasOwnProperty(message.guild.id)){
             let guildXpObject = userXpObject[message.guild.id];
-                let newXP = 500
+                let newXP = 1000
                 let currentXp = guildXpObject["userXP"];
                 let updatedXP = newXP + currentXp;
                 let newMoneys = 1
@@ -33,21 +33,40 @@ module.exports = async(client, message) => {
                 let updatedMoneys = newMoneys + currentMoneys;
                 let currentLevel = guildXpObject["userLevel"];
                 let updatedLevel = updateLevel(updatedXP);
-        // //if levelled up
-        //         if (currentLevel != updatedLevel){
-        //             if(currentLevel = 2){
-        //                 message.member.roles.remove("735218837057962058")
-        //                 if(updatedLevel = 3){
-        //                     const role = message.guild.roles.cache.get(role => role.id === "736668508129198170");
-        //                     message.member.roles.add(role);
-        //             }
-        //             }
-        //             message.channel.send(`${message.member} has levelled up.`);
-        //         }
+        //if levelled up
+                if (currentLevel != updatedLevel){
+                    xpObject[message.author.id][message.guild.id]["userLevel"] = updatedLevel;
+                    if(currentLevel = 2){
+                        message.member.roles.remove("735218837057962058");
+                        message.member.roles.add("736668508129198170");
+                        // message.channel.send(`${message.member} Just ditched the Noobz Role. iPhone Photographers Role Get!`);
+                    }
+                    //
+                    //need to fix these role assignments, level is being added to the json file
+                    //
+                    else if(currentLevel = 5){
+                        message.member.roles.remove("736668508129198170");
+                        message.member.roles.add("736676248884871231");
+                        // message.channel.send(`${message.member} Just ditched the iPhone Photographers Role. Soy Boys Role Get!`);
+                    }
+                   else if(currentLevel = 10){
+                        message.member.roles.remove("736676248884871231");
+                        message.member.roles.add("736676760971640892");
+                        // message.channel.send(`${message.member} Just ditched the Soy Boys Role. Crafty Crew Role Get!`);
+                        
+                    }
+                    else if(currentLevel = 15){
+                        message.member.roles.remove("736676760971640892");
+                        message.member.roles.add("736676859596767413");
+                        // message.channel.send(`${message.member} Just ditched the Crafty Crew Role. Epic Gamers Role Get!`);
+                    }
+                    else{ message.channel.send(`${message.member} has levelled up.`)};
+            }
+            else console.log(currentLevel);
         //update json file 
                 xpObject[message.author.id][message.guild.id]["userXP"] = updatedXP;
                 xpObject[message.author.id][message.guild.id]["userMoneys"] = updatedMoneys;
-                xpObject[message.author.id][message.guild.id]["userLevel"] = updatedLevel;
+                
                 
                 await fs.writeFile("userXP.json", JSON.stringify (xpObject, null, 4), "utf8").catch(err=> console.log(err));
             }
@@ -77,26 +96,23 @@ module.exports = async(client, message) => {
             await fs.writeFile("userXP.json", JSON.stringify (xpObject, null, 4), "utf8").catch(err=> console.log(err));
         }
     }
-    //
-    // you can change the levels if you want
-    //
-    function updateLevel(exp){
-        //start as Noobz
-        if(exp >= 0 && exp <= 1000) return 1;
-        else if(exp > 1000 && exp <= 2000) return 2;//iphone photographers (>2000)
-        else if(exp > 2000 && exp <= 3000) return 3;
-        else if(exp > 3000 && exp <= 4000) return 4;
-        else if(exp > 4000 && exp <= 5000) return 5;//Soy Boys (>5000)
-        else if(exp > 5000 && exp <= 6000) return 6;
-        else if(exp > 6000 && exp <= 7000) return 7;
-        else if(exp > 7000 && exp <= 8000) return 8;
-        else if(exp > 8000 && exp <= 9000) return 9;
-        else if(exp > 9000 && exp <= 10000) return 10; //Crafty Crew (>10000)
-        else if(exp > 10000 && exp <= 12000) return 11;
-        else if(exp > 12000 && exp <= 14000) return 12;
-        else if(exp > 14000 && exp <= 16000) return 13;
-        else if(exp > 16000 && exp <= 18000) return 14;
-        else if(exp > 18000 && exp <= 20000) return 15;
-        else if(exp > 20000) return 16; //Epic Gamers (>20000)
-    }
+}
+// declare levels system
+function updateLevel(updatedXP){
+    if(updatedXP >= 0 && updatedXP <= 1000) return 1;//start as Noobz
+    else if(updatedXP > 1000 && updatedXP <= 2000) return 2;//iphone photographers (>2000)
+    else if(updatedXP > 2000 && updatedXP <= 3000) return 3;
+    else if(updatedXP > 3000 && updatedXP <= 4000) return 4;
+    else if(updatedXP > 4000 && updatedXP <= 5000) return 5;//Soy Boys (>5000)
+    else if(updatedXP > 5000 && updatedXP <= 6000) return 6;
+    else if(updatedXP > 6000 && updatedXP <= 7000) return 7;
+    else if(updatedXP > 7000 && updatedXP <= 8000) return 8;
+    else if(updatedXP > 8000 && updatedXP <= 9000) return 9;
+    else if(updatedXP > 9000 && updatedXP <= 10000) return 10; //Crafty Crew (>10000)
+    else if(updatedXP > 10000 && updatedXP <= 12000) return 11;
+    else if(updatedXP > 12000 && updatedXP <= 14000) return 12;
+    else if(updatedXP > 14000 && updatedXP <= 16000) return 13;
+    else if(updatedXP > 16000 && updatedXP <= 18000) return 14;
+    else if(updatedXP > 18000 && updatedXP <= 20000) return 15;
+    else if(updatedXP > 20000) return 16; //Epic Gamers (>20000)
 }
